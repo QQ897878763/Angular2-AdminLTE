a42d23f2bed3cf43ee389fc7fce3143aa13cfa15
pengzhou <897878763@qq.com>
2018-11-28 00:41:56 +0800

    新增备注

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,19 +1,23 @@
-import { AdminDashboard2Component } from './../admin/admin-dashboard2/admin-dashboard2.component';
-import { AdminDashboard1Component } from './../admin/admin-dashboard1/admin-dashboard1.component';
-import { StarterComponent } from './../starter/starter.component';
-import { AdminComponent } from './../admin/admin.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { AdminDashboard2Component } from './../admin/admin-dashboard2/admin-dashboard2.component'
+import { AdminDashboard1Component } from './../admin/admin-dashboard1/admin-dashboard1.component'
+import { StarterComponent } from './../starter/starter.component'
+import { AdminComponent } from './../admin/admin.component'
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule, Routes } from '@angular/router'
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      { path: '', redirectTo: 'starter', pathMatch: 'full' },
-      { path: 'starter', component: StarterComponent },
-    ])
+    RouterModule.forRoot(
+      [
+        { path: '', redirectTo: 'starter', pathMatch: 'full' }, //默认路由
+        { path: 'starter', component: StarterComponent },
+        //{ path: '**', component: PageNotFoundComponent }, //404路由  
+      ],
+      { enableTracing: true }
+    )
   ],
   declarations: [],
-  exports: [ RouterModule]
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
